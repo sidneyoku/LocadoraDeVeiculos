e0037fda5f383e2c9e60e643df0d288b24570ade
sidneyoku <sidney.t.oku@gmail.com>
2021-04-08 21:06:43 -0300

    Projeto inicial

---
+++ locadoradeveiculos/src/main/java/br/ifpr/paranavai/locadoradeveiculos/locadoradeveiculos/LocadoradeveiculosApplication.java
@@ -0,0 +1,13 @@
+package br.ifpr.paranavai.locadoradeveiculos.locadoradeveiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocadoradeveiculosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocadoradeveiculosApplication.class, args);
+	}
+
+}
---
+++ locadoradeveiculos/src/test/java/br/ifpr/paranavai/locadoradeveiculos/locadoradeveiculos/LocadoradeveiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ifpr.paranavai.locadoradeveiculos.locadoradeveiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocadoradeveiculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
